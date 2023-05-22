0a972bb70705c13245bac501f1515592584b9133
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-22 19:34:53 +0700

    Add game entity

---
+++ GameFinder/Domain/Entities/Game.swift
@@ -0,0 +1,20 @@
+//
+//  Game.swift
+//  GameFinder
+//
+//  Created by Prasetya on 22/05/23.
+//
+
+import Foundation
+
+struct Game {
+    let id: Int
+    let slug: String
+    let name: String
+    let released: String
+    let tba: Bool
+    let backgroundImage: String
+    let rating: Int
+    let ratingTop: Int
+    let ratingsCount: Int
+}
