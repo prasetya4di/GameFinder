365777fd01aa5e074eb8ef85bf29ba108e571232
Prasetya Adi Arfanto <prasetyaarfanto@gmail.com>
2023-05-23 12:44:45 +0700

    Add get favorite games use case

---
+++ GameFinder/Domain/UseCases/GetFavoriteGames.swift
@@ -0,0 +1,24 @@
+//
+//  GetFavoriteGames.swift
+//  GameFinder
+//
+//  Created by Prasetya on 23/05/23.
+//
+
+import Foundation
+
+protocol GetFavoriteGames {
+    func call() throws -> [Game]
+}
+
+class GetGavoriteGamesImpl: GetFavoriteGames {
+    private let repository: GameRepository
+    
+    init(_ repository: GameRepository) {
+        self.repository = repository
+    }
+    
+    func call() throws -> [Game] {
+        return try repository.getFavoriteGames()
+    }
+}
